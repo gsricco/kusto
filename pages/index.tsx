2afa527cde6e87f223c2df492e60df7ed5e10a03
sergio <gsricco@yandex.ru>
2023-06-28 18:54:07 +0300

    feat: fixed new password

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import {NextPageWithLayout} from './_app';
 import {getLayout} from 'components/Layout/BaseLayout/BaseLayout';
+import Link from "next/link";
 
 const Home: NextPageWithLayout = () => (
   // <StyledWrapper>   // не нужно, т.к. wrapper у нас в  Layout.tsx
+  <>
     <Image
       src="/kusto.png"
       alt="Next.js Logo"
@@ -11,6 +13,15 @@ const Home: NextPageWithLayout = () => (
       height={180}
       priority
     />
+    <div>
+      <Link href={'/login'}>Login</Link>
+      <Link href={'/registration'}>Login</Link>
+      <Link href={'/recovery'}>Login</Link>
+      <Link href={'/newPassword'}>Login</Link>
+    </div>
+
+  </>
+
   // {/*</StyledWrapper>*/}
 );
 
