01ccc7b95e132379f4ee74ab2b9b7e6f1cc8e573
polyakog <gpolyakov77@gmail.com>
2023-06-29 13:17:27 +0400

    refac: change title in registration page

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -1,109 +1,4 @@
 import React from "react"
-<<<<<<< HEAD
-import { Formik } from "formik"
-import showPasswordBtn from "../../public/icons/eye-outline.svg"
-import hidePasswordBtn from "../../public/icons/eye-off-outline.svg"
-import googleIcon from "../../public/icons/google-svgrepo-com.svg"
-import githubIcon from "../../public/icons/github-svgrepo-com.svg"
-import { SignupSchema } from "utils/registrationValidation"
-import {
-  StyledBtn,
-  StyledContainer,
-  StyledErrorMsg,
-  StyledForm,
-  StyledShowPasswordBtn,
-  StyledSignIn,
-  StyledSignInWrapper,
-  StyledSocialMediaIcon,
-  StyledSocialMediaWrapper,
-  StyledText,
-  StyledTitle
-} from "styles/styles"
-import { getLayout } from "../../components/Layout/BaseLayout/BaseLayout"
-import Home from "../index"
-import { useShowPassword } from "assets/hooks/useShowPassword"
-import FormikField from "components/FormikWrapper/FormikField"
-import FormLabel from "components/FormikWrapper/FormLabel"
-import { Button, ThemeButton } from "components/Button/ui/Button"
-
-export default function Registration() {
-  const { passwordType, passwordConfirmationType, showPassword, showPasswordConfirmation } =
-    useShowPassword()
-
-  return (
-    <StyledContainer>
-      <StyledTitle>Sign Up</StyledTitle>
-      <StyledSocialMediaWrapper>
-        <StyledSocialMediaIcon alt="google-icon" src={googleIcon} />
-        <StyledSocialMediaIcon alt="github-icon" src={githubIcon} />
-      </StyledSocialMediaWrapper>
-      <Formik
-        initialValues={{
-          username: "",
-          password: "",
-          passwordConfirmation: "",
-          email: ""
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={async (values, { resetForm }) => {
-          console.log(values)
-          const data = {
-            email: values.email,
-            password: values.password,
-            login: values.username
-          }
-
-          try {
-            await fetch("https://calypso-one.vercel.app/auth/registration", {
-              method: "POST",
-              body: JSON.stringify(data),
-              headers: {
-                "Content-Type": "application/json"
-              }
-            }).then(() => console.log("otpravleno"))
-            resetForm()
-          } catch {
-            console.log("err")
-          }
-        }}
-      >
-        {({ errors, touched }) => (
-          <StyledForm>
-            <FormLabel>
-              Username
-              <FormikField
-                name="username"
-                border={errors.username?.length && touched.username ? "red" : "white"}
-              />
-              {errors.username && touched.username ? (
-                <StyledErrorMsg>{errors.username}</StyledErrorMsg>
-              ) : null}
-            </FormLabel>
-            <FormLabel id="pass">
-              Password
-              <FormikField
-                name="password"
-                type={passwordType}
-                border={errors.password?.length && touched.password ? "red" : "white"}
-              />
-              <StyledShowPasswordBtn
-                alt="show password"
-                src={passwordType === "password" ? showPasswordBtn : hidePasswordBtn}
-                onClick={() => showPassword()}
-              />
-              {errors.password && touched.password ? (
-                <StyledErrorMsg>{errors.password}</StyledErrorMsg>
-              ) : null}
-            </FormLabel>
-            <FormLabel id="pass">
-              Password confirmation
-              <StyledShowPasswordBtn
-                alt="show password"
-                src={passwordConfirmationType === "password" ? showPasswordBtn : hidePasswordBtn}
-                onClick={() => showPasswordConfirmation()}
-              />
-              <FormikField
-=======
 import {Formik} from "formik"
 import showPasswordBtn from "../../public/icons/eye-outline.svg"
 import hidePasswordBtn from "../../public/icons/eye-off-outline.svg"
@@ -159,7 +54,7 @@ export default function Registration() {
 
   return (
     <StyledContainerAuth>
-      <WrapperContainerAuth title={"Sing In"}>
+      <WrapperContainerAuth title={"Sing Up"}>
         <AuthIcons/>
         <Formik
           initialValues={initialAuthValues}
@@ -212,49 +107,16 @@ export default function Registration() {
               </FormikLabel>
               <FormikLabel
                 id="pass"
->>>>>>> dev
                 name="passwordConfirmation"
                 onChange={(e) => setFieldValue("passwordConfirmation", e)}
                 value={values.passwordConfirmation}
                 type={passwordConfirmationType}
-<<<<<<< HEAD
-=======
                 title={"Password confirmation"}
->>>>>>> dev
                 border={
                   errors.passwordConfirmation?.length && touched.passwordConfirmation
                     ? "red"
                     : "white"
                 }
-<<<<<<< HEAD
-              />
-              {errors.passwordConfirmation && touched.passwordConfirmation ? (
-                <StyledErrorMsg>{errors.passwordConfirmation}</StyledErrorMsg>
-              ) : null}
-            </FormLabel>
-            <FormLabel>
-              Email
-              <FormikField
-                name="email"
-                type="email"
-                border={errors.email?.length && touched.email ? "red" : "white"}
-              />
-              {errors.email && touched.email ? (
-                <StyledErrorMsg>{errors.email}</StyledErrorMsg>
-              ) : null}
-            </FormLabel>
-            <Button type="submit" theme={ThemeButton.PRIMARY}>
-              Sign Up
-            </Button>
-          </StyledForm>
-        )}
-      </Formik>
-      <StyledSignInWrapper>
-        <StyledText>Do you have an account?</StyledText>
-        <StyledSignIn href="/login">Sign in</StyledSignIn>
-      </StyledSignInWrapper>
-    </StyledContainer>
-=======
                 errors={errors}
                 touched={touched}
               >
@@ -276,7 +138,6 @@ export default function Registration() {
         </StyledSignInWrapper>
       </WrapperContainerAuth>
     </StyledContainerAuth>
->>>>>>> dev
   )
 }
 
