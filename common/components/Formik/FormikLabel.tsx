f1d266ca864b2972420251f2e142e3f6c6da9760
sergio <gsricco@yandex.ru>
2023-07-17 20:53:14 +0300

    feat: add logout

--- common/components/Formik/FormikLabel.tsx
+++ common/components/Formik/FormikLabel.tsx
@@ -1,8 +1,7 @@
-import { FormikField } from "./FormikField";
+import {FormikField} from "./FormikField";
 import React from "react";
-import styled from "styled-components";
-import { baseTheme } from "../../../styles/styledComponents/theme";
-import { labelType } from "./types";
+import {labelType} from "./types";
+import {StyledErrorMsg, StyledInputContainer, StyledLabel, StyledTitle} from "./Formik.styled";
 
 export const FormikLabel = ({
   title,
@@ -21,32 +20,6 @@ export const FormikLabel = ({
   marginBottom
 }: labelType) => {
 
-  // if (name === "username")
-  //   errors.username && touched.username ? (errorMessage = errors.username) : "";
-  // if (name === "email") errors.email && touched.email ? (errorMessage = errors.email) : "";
-  // if (name === "password")
-  //   errors.password && touched.password ? (errorMessage = errors.password) : "";
-  // if (name === "passwordConfirmation")
-  //   errors.passwordConfirmation && touched.passwordConfirmation
-  //     ? (errorMessage = errors.passwordConfirmation)
-  //     : "";
-  // if (name === "loginOrEmail")
-  //   errors.loginOrEmail && touched.loginOrEmail ? (errorMessage = errors.loginOrEmail) : "";
-  // if (name === "newPassword")
-  //   errors.newPassword && touched.newPassword ? (errorMessage = errors.newPassword) : "";
-  // if (name === "newPassword")
-  //   errors.newPassword && touched.newPassword ? (errorMessage = errors.newPassword) : "";
-  // if (name === "aboutMe") errors.aboutMe && touched.aboutMe ? (errorMessage = errors.aboutMe) : "";
-  // if (name === "recoveryCode")
-  //   errors.recoveryCode && touched.recoveryCode ? (errorMessage = errors.recoveryCode) : "";
-  // if (name === "firstname")
-  //   errors.firstname && touched.firstname ? (errorMessage = errors.firstname) : "";
-  // if (name === "lastname")
-  //   errors.lastname && touched.lastname ? (errorMessage = errors.lastname) : "";
-  // if (name === "birthday")
-  //   errors.birthday && touched.birthday ? (errorMessage = errors.birthday) : "";
-  // if (name === "city") errors.city && touched.city ? (errorMessage = errors.city) : "";
-
   return (
     <StyledLabel id={id} withError={!!errors[name] && !!touched[name]} marginBottom={marginBottom}>
       <StyledTitle>
@@ -62,7 +35,6 @@ export const FormikLabel = ({
           width={width}
           textAreaData={textAreaData}
         />
-
         {children}
       </StyledInputContainer>
       {!!errors[name] && touched[name] && (
@@ -71,45 +43,3 @@ export const FormikLabel = ({
     </StyledLabel>
   );
 };
-
-const StyledInputContainer = styled.div`
-  position: relative;
-`;
-
-const StyledLabel = styled.label<{ withError: boolean; marginBottom?: string }>`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  flex-shrink: 0;
-  font-family: Inter;
-  font-size: 16px;
-
-  margin-bottom: ${(props) => (props.withError ? 0 : props.marginBottom || "24px")};
-`;
-
-type StyledErrorMsgPropsType = {
-  errorShow?: boolean;
-};
-
-const StyledErrorMsg = styled.div<StyledErrorMsgPropsType>`
-  width: 100%;
-  font-family: Inter;
-  font-weight: 400;
-  letter-spacing: 0em;
-  text-align: left;
-  line-height: 24px;
-  font-size: 14px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-
-  color: ${baseTheme.colors.danger["500"]};
-`;
-
-const StyledTitle = styled(StyledErrorMsg)`
-  color: ${baseTheme.colors.light["900"]};
-
-  & span::first-letter {
-    text-transform: uppercase;
-  }
-`;
--- common/components/Navbar/AppLink/AppLink.tsx
+++ common/components/Navbar/AppLink/AppLink.tsx
@@ -1,14 +1,12 @@
 import {FC, ReactNode} from 'react';
 import Link, {LinkProps} from 'next/link';
-import classNames from '../../../assets/lib/classNames/classNames';
-import cls from './AppLink.module.css'
 
 export enum ThemeAppLink {
   PRIMARY = 'primary',
 }
 
 interface AppLinkProps extends LinkProps {
-  className?: string
+  // className?: string
   theme?: ThemeAppLink
   children: ReactNode
 }
@@ -16,16 +14,14 @@ interface AppLinkProps extends LinkProps {
 export const AppLink: FC<AppLinkProps> = (props) => {
   const {
     href,
-    className,
     children,
-    theme = ThemeAppLink.PRIMARY,
     ...otherProps
   } = props;
 
   return (
     <Link
       href={href}
-      className={classNames(cls.AppLink, {}, [className, theme])}
+      // className={classNames(cls.AppLink, {}, [className, theme])}
       {...otherProps}
     >
       {children}
