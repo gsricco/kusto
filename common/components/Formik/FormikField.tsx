15c65b42a467337584d27e1500f68ac5b34e3525
BogdanKurenkov <badikradik123@gmail.com>
2023-07-25 12:46:40 +0300

    fixed texrarea error on settings page

--- common/components/Formik/FormikField.tsx
+++ common/components/Formik/FormikField.tsx
@@ -1,18 +1,21 @@
-import {ChangeEvent} from "react";
-import {FiledProps} from "./types";
-import {StyledField} from "./Formik.styled";
-import {FieldTextarea} from "./FieldTextarea";
+import { ChangeEvent } from "react";
+import { FiledProps } from "./types";
+import { StyledField } from "./Formik.styled";
+import { FieldTextarea } from "./FieldTextarea";
 
 export const FormikField = (props: FiledProps) => {
-  return props.type !== "textarea"
-    ? <StyledField
+  return props.type !== "textarea" ? (
+    <StyledField
       {...props}
       onChange={(e: ChangeEvent<HTMLInputElement>) => props.onChange(e.target.value)}
       width={props.width}
     />
-    : <FieldTextarea
+  ) : (
+    <FieldTextarea
       {...props}
       onChange={(e) => props.onChange(e.target.value)}
       width={props.width}
+      value={props.value}
     />
+  );
 };
