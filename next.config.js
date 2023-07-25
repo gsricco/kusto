131c451f063c8b082633c5def2890b1f1c2db9fa
BogdanKurenkov <badikradik123@gmail.com>
2023-07-25 15:17:33 +0300

    add next config rules

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config.js')    // настройка для работы библиотеки по смене языка
+const { i18n } = require("./next-i18next.config.js"); // настройка для работы библиотеки по смене языка
 const nextConfig = {
-  i18n,                                 
+  i18n,
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'storage.yandexcloud.net',
-        port: '',
-        pathname: '/**',
-      },
+        protocol: "https",
+        hostname: "storage.yandexcloud.net",
+        port: "",
+        pathname: "/**"
+      }
     ]
+  },
+  compiler: {
+    styledComponents: true
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
