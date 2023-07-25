7b0da625ef23f49c110aa2e93ad7fe92e047373d
BogdanKurenkov <badikradik123@gmail.com>
2023-07-25 14:49:57 +0300

    fix errors

--- common/components/Navbar/MainLink/MainLink.tsx
+++ common/components/Navbar/MainLink/MainLink.tsx
@@ -7,15 +7,15 @@ interface SidebarLinkProps {
   src: string;
   name: string;
   href: string;
-  isActive: boolean;
+  isactive: string;
 }
 
-export const MainLink: FC<SidebarLinkProps> = ({ name, src, href, isActive }) => {
+export const MainLink: FC<SidebarLinkProps> = ({ name, src, href, isactive }) => {
   return (
     <AppLink href={href}>
       <StyledDiv>
         <Image src={src} alt={"some icon"} width={24} height={24} />
-        <StyledText isActive={isActive}>{name}</StyledText>
+        <StyledText isactive={isactive}>{name}</StyledText>
       </StyledDiv>
     </AppLink>
   );
