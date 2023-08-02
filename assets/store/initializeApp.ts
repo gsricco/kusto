6001d472431e66722d94abd1b32bbe3c197f1b4a
polyakog <gpolyakov77@gmail.com>
2023-08-02 16:15:43 +0400

    feat: add initializeApp

---
+++ assets/store/initializeApp.ts
@@ -0,0 +1,36 @@
+import { appActions } from "./app-reducer";
+import { MeType } from "./api/auth/types";
+import { batch } from "react-redux";
+import { AppDispatch } from "./store";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
+import { SerializedError } from "@reduxjs/toolkit";
+
+export const initializeApp = (
+  me: MeType | undefined,
+  isLoading: boolean,
+  error: FetchBaseQueryError | SerializedError | undefined,
+  dispatch: AppDispatch
+) => {
+  switch (true) {
+    case isLoading:
+      batch(() => {
+        dispatch(appActions.setIsLoading({ isLoading: true }));
+      });
+      break;
+    case !!me:
+      batch(() => {
+        dispatch(appActions.setIsAppInitialized({ isAppInitialized: true }));
+        dispatch(appActions.setUser({ me: me! }));
+        dispatch(appActions.setIsLoading({ isLoading: false }));
+      });
+      break;
+    case !!error:
+      batch(() => {
+        dispatch(appActions.setError({ error: error }));
+        dispatch(appActions.setIsAppInitialized({ isAppInitialized: false }));
+        dispatch(appActions.setIsLoading({ isLoading: false }));
+      });
+    default:
+      break;
+  }
+};
