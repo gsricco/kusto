dd34872dbd3bb046ed0f6174141f5402369f018e
Bogdan <badikradik123@gmail.com>
2023-08-22 11:03:18 +0300

    fix: removed unused key property

--- features/posts/FilterElement.tsx
+++ features/posts/FilterElement.tsx
@@ -3,44 +3,38 @@ import Canvas from "./Canvas";
 import { useState } from "react";
 import Image from "next/image";
 
-
 /// //  Элемент, отображающий изображение с наложенным фильтром   /// //
 
-const FilterElement = ({ 
-    key,
-    photoUrl,
-    filterTitle,
-    filter,
-    handleFilter
-  }: {
-    key: number
-    photoUrl: string
-    filterTitle: string
-    filter: string
-    handleFilter: (filterTitle: string) => void
-  }) => {
+const FilterElement = ({
+  photoUrl,
+  filterTitle,
+  filter,
+  handleFilter
+}: {
+  photoUrl: string;
+  filterTitle: string;
+  filter: string;
+  handleFilter: (filterTitle: string) => void;
+}) => {
+  const handleFilterChose = () => {
+    handleFilter(filter);
+  };
 
-    const handleFilterChose = () => {
-      handleFilter(filter)
-    }
+  return (
+    <StyledModalBody onClick={handleFilterChose}>
+      <Image
+        src={photoUrl}
+        width={108}
+        height={108}
+        alt="nolmal"
+        style={{ objectFit: "contain", filter: filter }}
+      />
+      <StyledFilterTitle>{filterTitle}</StyledFilterTitle>
+    </StyledModalBody>
+  );
+};
 
-    return (
-      <StyledModalBody onClick={handleFilterChose}>
-        <Image
-            src={photoUrl}
-            width={108}
-            height={108}
-            alt="nolmal"
-            style={{ objectFit: "contain", filter: filter }}
-          />
-        <StyledFilterTitle>
-          {filterTitle}
-        </StyledFilterTitle>
-      </StyledModalBody>  
-    )
-}
-
-export default FilterElement
+export default FilterElement;
 
 const StyledModalBody = styled.div`
   display: flex;
@@ -51,7 +45,6 @@ const StyledModalBody = styled.div`
 `;
 
 const StyledFilterTitle = styled.div`
-
   width: 100%;
   max-width: 160px;
   text-align: center;
